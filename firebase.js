53921b72d0973e2fa237639f747ccf1afd97ac36
Mona9324 <qoekfthsu9@gmail.com>
2026-03-15 18:28:11 +0900

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+
+apiKey:  "AIzaSyD-ctq-wOobkLAQtGb7keGIOQZBiH05f5M",
+authDomain: "wos-svs-booking.firebaseapp.com",
+projectId: "wos-svs-booking",
+
+};
+
+firebase.initializeApp(firebaseConfig)
+
+const db = firebase.firestore()
+
