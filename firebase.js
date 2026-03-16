1ae6c30c45c74c04728a520e082d5718cab274f2
Mona9324 <qoekfthsu9@gmail.com>
2026-03-16 15:12:31 +0900

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
 const firebaseConfig = {
-apiKey: "AIzaSyA90OVkTb5svXjHrF37wI_0dUWv-4Pr7Xc",
-authDomain: "svs-booking-4a785.firebaseapp.com",
-projectId: "svs-booking-4a785",
-storageBucket: "svs-booking-4a785.firebasestorage.app",
-messagingSenderId: "815196694859",
-appId: "1:815196694859:web:eb39b55526841c248860ce"
+  apiKey: "AIzaSyA90OVkTb5svXjHrF37wI_0dUWv-4Pr7Xc",
+  authDomain: "svs-booking-4a785.firebaseapp.com",
+  projectId: "svs-booking-4a785",
+  storageBucket: "svs-booking-4a785.firebasestorage.app",
+  messagingSenderId: "815196694859",
+  appId: "1:815196694859:web:eb39b55526841c248860ce"
 };
 
 if (!firebase.apps.length) {
-firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 }
 
 window.db = firebase.firestore();
